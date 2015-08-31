72fdbd98927e7d1bff9d108ce3d0317fe56044b4
mattshanker <matt@mattshanker.com>
2015-08-31 19:06:25 -0400

    gulpfile edits

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
+var s = require('browser-sync').create();
 
 // Static server
-gulp.task('browser-sync', function() {
+gulp.task('serve', function() {
     browserSync.init({
         server: {
             baseDir: "./"
+            browser:["google chrome", "firefox"]
         }
     });
+    gulp.watch("./css/*.css").on('change', browserSync.reload);
+    gulp.watch("./*.html").on('change', browserSync.reload);
 });
