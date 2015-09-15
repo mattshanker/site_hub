a63d295087de173d596be18efcda092520199acf
mattshanker <matt@mattshanker.com>
2015-09-15 12:46:01 -0400

    added svg header, layout edits

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,16 @@ var gulp          = require('gulp');
 var sync          = require('browser-sync').create();
 var autoprefixer  = require('gulp-autoprefixer');
 // build task
+//todo
+//do something to imgs on build
+//open more browsers on serve
 gulp.task('build', function () {
     return gulp.src('css/styles.css')
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
         }))
-        .pipe(gulp.dest('./css/'));        
+        .pipe(gulp.dest('./css/'));
 });
 // Static server
 gulp.task('serve', function() {
