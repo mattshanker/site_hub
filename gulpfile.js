bb81fa715c36d1dee747c440f4e393da1c254e86
mattshanker <matt@mattshanker.com>
2015-10-07 17:05:00 -0400

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp          = require('gulp');
 var sync          = require('browser-sync').create();
 var autoprefixer  = require('gulp-autoprefixer');
+var sass          = require('gulp-sass');
 // build task
 //todo
 //do something to imgs on build
@@ -17,11 +18,18 @@ gulp.task('build', function () {
 gulp.task('serve', function() {
     sync.init({
         server: {
-            baseDir: "./"
-        //browser: {
-        // "google chrome, firefox, Safari"
-      //}
-      }
-    });
-    gulp.watch(["./*.html", "./css/*.css", "./js/*.js"]).on('change', sync.reload);
+            baseDir: "./",
+            browser: "google chrome, firefox, Safari",
+            notify: false,
+  }
+});
+    gulp.watch(["./*.html", "./css/*.css", "./css/*.scss", "./js/*.js"]).on('change', sync.reload);
+});
+//concat/min/gzip html/css/js
+
+//compile scss
+gulp.task('sass', function () {
+  gulp.src('./css/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
 });
