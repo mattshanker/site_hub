3a85304b470962330e00143434d003dbe8924f20
Matt <matt@mattshanker.com>
2016-01-12 01:56:14 -0500

    remove gsap, add velocity via cdn

--- js/main.js
+++ js/main.js
@@ -1,100 +1,48 @@
 
-/**
- * main.js
- * http://www.codrops.com
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- *
- * Copyright 2014, Codrops
- * http://www.codrops.com
- */
 
-  var mo = document.getElementById("modal"),
-			co = document.getElementById("modal-content"),
-			c = document.getElementById('clsbtn'),
-			h = document.getElementById('header'),
-			tl = new TimelineMax(),
-      abo = document.getElementById("about"),
-			mot = document.getElementById('motrig'),
-			dev = document.getElementById('modalt1'),
-			inb = document.getElementById('modalt2'),
-			ser = document.getElementById('services'),
-			devs = document.getElementById('devel'),
-			inbs = document.getElementById('inbound'),
-			wo = document.getElementById('btnWork'),
-			se = document.getElementById('btnServices'),
-			con = document.getElementById('btnContact'),
-			wos = document.getElementById('work'),
-			sco = document.getElementById('scone'),
-			sct = document.getElementById('sctwo'),
-			devp = sco.getElementsByTagName('p'),
-			inbp = sct.getElementsByTagName('p'),
-			h = document.getElementById('htitle');
+var h = $('#htitle');
 
-function clmo () {
-		tl.to([mo, co, h, mot], 0.5, {css: {className:"-=modal-is-visible"}});
-	}
-
-function wid () {
-		var cssw = window.innerWidth;
-
-			h.innerHTML = cssw;
-	}
-
-	document.addEventListener("DOMContentLoaded", function () {
-		tl.to(ser, 0.3, {css: {display: "none"}});
-		console.log('services hidden');
-	}, false);
-
-	se.onclick = function (e) {
-  e.preventDefault();
-  tl.to([inbp, inbs, wos, abo], 0.3, {css: {display: "none", top:"100"}});
-		tl.to(ser, 0.3, {css:{display:"block", top:"-100"}});
-		console.log('services!');
-	};
-
-
-	//se.addEventListener('click', showSer, false);
-
-	mot.addEventListener('click', showModal, false);
-
-		function showModal (e) {
-		e.preventDefault();
-			tl.to([mo, h, mot, co], 0.5, {css: {className:"+=modal-is-visible"}, ease:Bounce.easeOut});
-			// tl.to(co, 0.5, {visibility:'visible', top: '0', ease:Bounce.easeOut});
-				console.log('modal toggled');
-	}
-
-	c.onclick = function (e) {
+ 	$("#modalt1").click(function (e) {
  		e.preventDefault();
- 		clmo();
-			console.log('modal closed');
- 	};
+    $('#inbound').velocity({
+     top:"100"
+   },{
+     display: "none",
+   });
 
- 	dev.onclick = function (e) {
+      $("#devel").velocity({
+        top:"-100"
+      },{
+        display: "block",
+          easing:"easeOut"
+        });
 
- 		e.preventDefault();
- 			tl.to([inbp, inbs, wos], 0.3, {css: {display: "none", top:"100"}});
- 			tl.to([devs, devp], 0.5, {css: {display: "block", top:"-100"}, ease:Power1.easeOut});
- 		h.innerText = 'web dev';
+    h.innerText = 'web dev';
                console.log("dev clicked");
- 	};
+ 	});
 
- 	inb.onclick = function (e) {
+ 	$("#modalt2").click(function (e) {
  		e.preventDefault();
- 			tl.to([devp, devs, wos], 0.5, {css: {display: "none", top: "100"}});
- 			tl.to([inbs, inbp], 0.5, {css: {display: "block", top: "-100"}, ease:Power1.easeOut});
- 		h.innerHTML = 'inbound mktg';
- 	};
+    $("#inbound").velocity({
+      top: "-100"
+    },{ duration: 500,
+      display: "block",
+      easing: "easeInOut",
+      complete: console.log('inbound section triggered')
+    });
+ 			$("#scone").velocity({
+        opacity: "0",
+        top: "100"
+      },{
+        duration: 300,
+        display:"none",
+        complete:function() {console.log('dev section is gone!')}
+      }
+    );
+    $('#services').velocity({
+      backgroundColor:"#9FB8AD"
+    });
 
- 	wo.onclick = function (e) {
- 		e.preventDefault();
- 			tl.to([devp, inbp, devs, inbs, ser], 0.3, {css:{display:"none", top: "100"}});
- 			tl.to(wos, 0.3, {css:{display:"block", top: "-100"}});
- 		h.innerHTML = 'work';
- 	};
+ 	});
 
-
- 	wid();
  	console.log('functional function!');
