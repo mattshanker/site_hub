31fb22b84d0d7db3bb7fa8d257df4e85be0031de
Matt <matt@mattshanker.com>
2016-01-13 05:27:05 -0500

    css cleanup

--- js/main.js
+++ js/main.js
@@ -1,31 +1,61 @@
 
 
-var h = $('#htitle');
+var ht = $('#htitle');
+var h = $('#header');
 
- 	$("#modalt1").click(function (e) {
+$(window).load(function (){
+  $('#work').toggleClass('hide');
+  console.log('work section hidden')
+});
+$('#btnWork').click(function (e){
+  e.preventDefault();
+  $('#work').toggleClass('hide');
+  console.log('show work');
+});
+
+$("#modalt1").click(function (e) {
  		e.preventDefault();
     $('#inbound').velocity({
-     top:"100"
+      top:"100"
    },{
-     display: "none",
+      display: "none",
    });
-
-      $("#devel").velocity({
-        top:"-100"
-      },{
-        display: "block",
-          easing:"easeOut"
+    $("#devel").velocity({
+      top:"-100"
+    },{
+      duration: 500,
+      display: "block",
+      easing:"easeOut"
         });
-
-    h.innerText = 'web dev';
-               console.log("dev clicked");
+  $(h).velocity({
+    backgroundColor: "#1976D2"
+  },{
+    complete: function(){$(ht).text('web dev');}
+  });
+    console.log("dev clicked");
  	});
 
  	$("#modalt2").click(function (e) {
  		e.preventDefault();
+  var d = $('#devel').css('display');
+  console.log(d);
+  if (d == 'block'|'flex') {
+    $("#devel").velocity({
+      top:"100"
+    },{
+      duration: 300,
+      display: "",
+      easing:"easeOut"
+        });
+    console.log('display is none')
+  } else{
+    $('#devel').toggleClass('hide');
+    console.log('display is block')
+  }
     $("#inbound").velocity({
       top: "-100"
-    },{ duration: 500,
+    },{
+      duration: 500,
       display: "block",
       easing: "easeInOut",
       complete: console.log('inbound section triggered')
@@ -39,10 +69,13 @@ var h = $('#htitle');
         complete:function() {console.log('dev section is gone!')}
       }
     );
-    $('#services').velocity({
+    $(h).velocity({
       backgroundColor:"#9FB8AD"
+    },{
+      complete:function () {$(ht).html("Inbound")}
     });
 
  	});
+
 
  	console.log('functional function!');
