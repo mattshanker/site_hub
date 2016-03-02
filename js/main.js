6d773002848e277dba490ae4bf6e12afc1b9ac8e
Matt Shanker <matt@mattshanker.com>
2016-03-01 22:34:38 -0500

    removed section classes, most breakpoints

--- js/main.js
+++ js/main.js
@@ -1,72 +1,12 @@
-
-
 var ht = $('#htitle');
 var h = $('#header');
 
 $(window).load(function (){
   $('#nav').addClass('hide');
-  
 });
 
 $("#toggle").click(function(){
   $("#nav").toggleClass('hide');
 });
 
-$("#modalt1").click(function (e) {
-	e.preventDefault();
-    $('#inbound').velocity({
-      top:"100"
-   },{
-      display: "none",
-   });
-    $("#devel").velocity({
-      top:"-90"
-    },{
-      duration: 500,
-      display: "block",
-      easing:"easeOut"
-        });
-  $(h).velocity({
-    backgroundColor: "#1976D2"
-  },{
-    complete: function(){$(ht).text('web dev');}
-  });
-    console.log("dev clicked");
- 	});
-
- 	$("#modalt2").click(function (e) {
- 		e.preventDefault();
-  var d = $('#devel').css('display');
-  console.log(d);
-  if (d == 'block'|'flex') {
-    $("#devel").velocity({
-      top:"100"
-    },{
-      duration: 300,
-      display: "",
-      easing:"easeOut"
-        });
-    console.log('display is none')
-  } else{
-    $('#devel').fadeToggle('hide');
-    console.log('display is block')
-  }
-    $("#inbound").velocity({
-      top: "-90"
-    },{
-      duration: 500,
-      display: "block",
-      easing: "easeInOut",
-      complete: console.log('inbound section triggered')
-    });
-
-    $(h).velocity({
-      backgroundColor:"#9FB8AD"
-    },{
-      complete:function () {$(ht).html("Inbound")}
-    });
-
- 	});
-
-
- 	console.log('functional function!');
+console.log('functional function!');
