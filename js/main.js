f1392f799d4c9f9e1242663f15b346b7e02c291c
Matt Shanker <matt@mattshanker.com>
2016-04-04 23:35:36 -0400

    shrunk gif, added svg icons, remove some css

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 
-
-$("#toggle").click(function(){
-  $("#nav").toggleClass('hide');
-  $("#toggle").toggleClass('open');
+window.addEventListener("load",function() {
+  setTimeout(function(){
+	  window.scrollTo(0, 1);
+  }, 0);
 });
 
 console.log('Thank you for visiting!');
